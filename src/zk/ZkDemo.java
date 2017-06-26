fb6faaa036b431169df3f9404031c854b3763eca
jinyueyuan <jinyueyuan@outlook.com>
2017-06-26 17:39:39 +0800

    first commit

---
+++ src/zk/ZkDemo.java
@@ -0,0 +1,71 @@
+package zk;
+
+/**
+ * Created by yueyuan on 09/05/2017.
+ */
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooDefs.Ids;
+
+public class ZkDemo {
+
+    public static int CLIENT_PORT = 2181;
+    public static int TIME_OUT    = 2000;
+//192.168.0.184:
+    public static void main(String args[]) throws Exception {
+        // 创建一个与服务器的连接
+    	hello();
+    }
+public static void hello() throws Exception{
+	 System.out.println("aaaa");
+	ZooKeeper zk = new ZooKeeper("192.168.0.184:" + CLIENT_PORT,
+            TIME_OUT, new Watcher() {
+        // 监控所有被触发的事件
+        public void process(WatchedEvent event) {
+            System.out.println("已经触发了" + event.getType() + "事件！");
+        }
+    });
+
+    // 创建一个目录节点
+    zk.create("/testRootPath", "testRootData".getBytes(), Ids.OPEN_ACL_UNSAFE,
+            CreateMode.PERSISTENT);
+
+    // 创建一个子目录节点
+    zk.create("/testRootPath/testChildPathOne", "testChildDataOne".getBytes(),
+            Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
+    System.out.println(new String(zk.getData("/testRootPath",false,null)));
+
+    // 取出子目录节点列表
+    System.out.println(zk.getChildren("/testRootPath",true));
+
+    // 修改子目录节点数据
+    zk.setData("/testRootPath/testChildPathOne","modifyChildDataOne".getBytes(),-1);
+    System.out.println("目录节点状态：["+zk.exists(    "/testRootPath",true)+"]");
+
+    // 创建另外一个子目录节点
+    zk.create("/testRootPath/testChildPathTwo", "testChildDataTwo".getBytes(),
+            Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
+    System.out.println(new String(zk.getData("/testRootPath/testChildPathTwo",true,null)));
+
+    // 创建一个临时子目录节点
+    zk.create("/testRootPath/testEphemeral", "testEphemeral".getBytes(),
+            Ids.OPEN_ACL_UNSAFE,CreateMode.EPHEMERAL);
+
+    // 删除子目录节点
+    zk.delete("/testRootPath/testChildPathTwo",-1);
+    zk.delete("/testRootPath/testChildPathOne",-1);
+    zk.delete("/testRootPath/testEphemeral",-1);
+    if(zk.exists("/testRootPath", false)!=null)
+    	 // 取出子目录节点列表
+        //System.out.println(zk.getChildren("/testRootPath",true));
+        zk.delete("/testRootPath",-1);
+    else 
+    	System.out.println("结点不存在");
+
+    // 关闭连接
+    zk.close();
+}
+}
